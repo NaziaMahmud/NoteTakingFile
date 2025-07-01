7e94b548f54e56df2f697d36a2655c22ef390b02
shiu.wong <shiuwong1122@gmail.com>
2025-07-01 14:50:28 -0400

    updates

---
+++ backend.js
@@ -0,0 +1,29 @@
+import bodyParser from "body-parser";
+import express from "express";
+import path from "path";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+//app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "frontend")));
+
+let notes = []; // To store your note objects
+let nextNoteId = 1; // To assign unique IDs to new notes
+
+app.get("/", function(req, res){
+    console.log("testing");
+    res.sendFile(path.join(__dirname, "frontend", "index.html"))
+    });
+
+// app.post("/", function(req, res) {
+//     console.log("testing");
+//     res.sendFile(path.join(__dirname, "NoteTakingFile", "index.html"))
+//     });
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
