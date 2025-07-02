5ebbe9c29acdc9a2b31cc6f8a937c5b4d03579a3
shiu.wong <shiuwong1122@gmail.com>
2025-07-02 12:07:25 -0400

    changes

--- backend.js
+++ backend.js
@@ -1,29 +1,37 @@
 import bodyParser from "body-parser";
 import express from "express";
 import path from "path";
-import {dirname} from "path";
-import {fileURLToPath} from "url";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "frontend")));
 
 let notes = []; // To store your note objects
 let nextNoteId = 1; // To assign unique IDs to new notes
 
-app.get("/", function(req, res){
-    console.log("testing");
-    res.sendFile(path.join(__dirname, "frontend", "index.html"))
-    });
+app.get("/", function (req, res) {
+  console.log("testing1");
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
 
-// app.post("/", function(req, res) {
-//     console.log("testing");
-//     res.sendFile(path.join(__dirname, "NoteTakingFile", "index.html"))
-//     });
+app.post("/", function (req, res) {
+  console.log("testing");
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
 
+app.put("/", function (req, res) {
+  console.log("testing");
+  res.sendFile(path.join(__dirname,"frontend", "index.html"));
+});
+app.delete("/", function (req, res) {
+  console.log("testing");
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
