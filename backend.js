2d01b10093e776a612bbc00da927796f97b47421
shiu.wong <shiuwong1122@gmail.com>
2025-07-03 10:03:45 -0400

    updates

--- backend.js
+++ backend.js
@@ -12,7 +12,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "frontend")));
 
 let notes = []; 
-let nextNoteId = 1; 
 
 app.get("/", function (req, res) {
   console.log("testing1");
